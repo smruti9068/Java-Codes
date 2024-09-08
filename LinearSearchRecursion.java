4afdde3d01258508bce97d8db013205c334913a3
smruti9068 <113612132+smruti9068@users.noreply.github.com>
2024-09-08 18:54:59 +0530

    Add files via upload

---
+++ LinearSearchRecursion.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class LinearSearchRecursion 
+{
+    public static void main(String[] args) 
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter the size of array and elements :");
+        int n = obj.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = obj.nextInt();
+         System.out.print("Enter search element :");
+        int key = obj.nextInt();
+        System.out.println("Element present in the array : "+Search(arr,key,0));
+    }
+    public static boolean Search(int[] a,int x,int i)
+    {
+        if (i==a.length)
+            return false;
+        if(a[i]==x)
+            return true;                
+        return Search(a,x,i+1);
+    }
+    
+}
